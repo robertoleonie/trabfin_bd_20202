64f1b1c61bfadddea3008f4cf01e3b3d72427e33
JaoSchmidt <jhsc98@gmail.com>
2021-06-08 03:43:00 -0300

    comentando tabelas com unique keys, add engine InnoDB
    Eu consegui rodar no meu pc, testem no windows de vcs pf

--- query_relacionamentos.sql
+++ query_relacionamentos.sql
@@ -1,15 +1,31 @@
+begin;
 SET GLOBAL log_bin_trust_function_creators = 1;
 use vodan_br_bd;
 
-DROP TABLE if exists `tb_QuestionsType`;
-CREATE TABLE `tb_questionstype` (
-	`questionTypeID` int NOT NULL AUTO_INCREMENT,
-	`description` varchar(255) NOT NULL COMMENT '(pt-br) Descrição.\r\n(en) description.',
-	 PRIMARY KEY (`questionTypeID`)
-);
+ALTER TABLE `tb_userrole` ENGINE=InnoDB;
+ALTER TABLE `tb_user` ENGINE=InnoDB;
+ALTER TABLE `tb_questiontype` ENGINE=InnoDB;
+ALTER TABLE `tb_questions` ENGINE=InnoDB;
+ALTER TABLE `tb_questionnaire` ENGINE=InnoDB;
+ALTER TABLE `tb_questiongroupformrecord` ENGINE=InnoDB;
+ALTER TABLE `tb_questiongroupform` ENGINE=InnoDB;
+ALTER TABLE `tb_questiongroup` ENGINE=InnoDB;
+ALTER TABLE `tb_permission` ENGINE=InnoDB;
+ALTER TABLE `tb_participant` ENGINE=InnoDB;
+ALTER TABLE `tb_notificationrecord` ENGINE=InnoDB;
+ALTER TABLE `tb_multilanguage` ENGINE=InnoDB;
+ALTER TABLE `tb_listtype` ENGINE=InnoDB;
+ALTER TABLE `tb_listofvalues` ENGINE=InnoDB;
+ALTER TABLE `tb_language` ENGINE=InnoDB;
+ALTER TABLE `tb_hospitalunit` ENGINE=InnoDB;
+ALTER TABLE `tb_grouprolepermission` ENGINE=InnoDB;
+ALTER TABLE `tb_grouprole` ENGINE=InnoDB;
+ALTER TABLE `tb_formrecord` ENGINE=InnoDB;
+ALTER TABLE `tb_crfforms` ENGINE=InnoDB;
+ALTER TABLE `tb_assessmentquestionnaire` ENGINE=InnoDB;
 
 ALTER TABLE `tb_questions`
-	ADD FOREIGN KEY (`questionTypeID`) REFERENCES `tb_questionstype` (`questionTypeID`),
+	ADD FOREIGN KEY (`questionTypeID`) REFERENCES `tb_questiontype` (`questionTypeID`),
 	ADD FOREIGN KEY (`listTypeID`) REFERENCES `tb_listofvalues` (`listTypeID`),
 	ADD FOREIGN KEY (`questionGroupID`) REFERENCES `tb_questiongroup` (`questionGroupID`);
     
@@ -38,17 +54,22 @@ ALTER TABLE `tb_questiongroupformrecord`
     
 ALTER TABLE `tb_multilanguage`
 	ADD FOREIGN KEY (`languageID`) REFERENCES `tb_language` (`languageID`);
-    
-ALTER TABLE `tb_grouprolepermission`
-	ADD FOREIGN KEY (`groupRoleID`) REFERENCES `tb_grouprole` (`groupRoleID`),
-    ADD FOREIGN KEY (`permissionID`) REFERENCES `tb_permission` (`permissionID`);
+
+-- Dando erro de compatibilidade em todas essas linhas
+-- Algumas tabelas possuem UNIQUE keys, que impedem FK
+-- ALTER TABLE `tb_grouprolepermission`
+--	ADD FOREIGN KEY (`groupRoleID`) REFERENCES `tb_grouprole` (`groupRoleID`);
+--	ADD FOREIGN KEY (`permissionID`) REFERENCES `tb_permission` (`permissionID`);
     
 ALTER TABLE `tb_userrole`
-	ADD FOREIGN KEY (`userID`) REFERENCES `tb_user` (`userID`),
-    ADD FOREIGN KEY (`groupRoleID`) REFERENCES `tb_grouprole` (`groupRoleID`),
-    ADD FOREIGN KEY (`hospitalUnitID`) REFERENCES `tb_hospitalunit` (`hospitalUnitID`);
+	ADD FOREIGN KEY (`hospitalUnitID`) REFERENCES `tb_hospitalunit` (`hospitalUnitID`);
     
+--	ADD FOREIGN KEY (`userID`) REFERENCES `tb_user` (`userID`),
+-- ADD FOREIGN KEY (`groupRoleID`) REFERENCES `tb_grouprole` (`groupRoleID`),
+
 ALTER TABLE `tb_notificationrecord`
-	ADD FOREIGN KEY (`userID`) REFERENCES `tb_user` (`userID`),
-	ADD FOREIGN KEY (`profileid`) REFERENCES `tb_grouprole` (`groupRoleID`),
     ADD FOREIGN KEY (`hospitalUnitID`) REFERENCES `tb_hospitalunit` (`hospitalUnitID`);
+
+-- ADD FOREIGN KEY (`userID`) REFERENCES `tb_user` (`userID`),
+-- ADD FOREIGN KEY (`profileid`) REFERENCES `tb_grouprole` (`groupRoleID`),
+commit;
